f713eeee836bfa3743bd7e9b4c1e6f3fc11d2986
Rafael Aarón <69669283+Noraa08@users.noreply.github.com>
2023-03-10 10:31:05 -0300

    Create

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+
+    name='anime.py',
+
+    version=__version__,
+
+    url='https://github.com/MoonlightGroup/anime.py',
+
+    author='Noraa08',
+
+    author_email='rafael.noraa08@gmail.com',
+
+    py_modules=['animepy'],
+
+)
